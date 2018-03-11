bd3079de948c07690f83ccc978929036532d61d9
Ivan Malison <IvanMalison@gmail.com>
2018-03-10 20:18:39 -0800

    Add StatusNotifierTray

---
+++ src/System/Taffybar/Widgets/StatusNotifierTray.hs
@@ -0,0 +1,47 @@
+module System.Taffybar.Widgets.StatusNotifierTray where
+
+import           Control.Monad
+import qualified Data.ByteString as BS
+import           Data.ByteString.Unsafe
+import           Data.Word
+import           Foreign.C.Types (CUChar(..))
+import           Foreign.Marshal.Array
+import           Foreign.Ptr
+import           Foreign.Storable
+import qualified Graphics.UI.Gtk as Gtk
+import           StatusNotifier.Host.Service as H
+
+sampleBits :: Int
+sampleBits = 8
+
+hasAlpha :: Bool
+hasAlpha = True
+
+colorspace :: Gtk.Colorspace
+colorspace = Gtk.ColorspaceRgb
+
+buildTray :: IO Gtk.Widget
+buildTray = do
+  box <- Gtk.hBoxNew False 5
+  let updateHandler NewItem info =
+        do
+          putStrLn "Handling new item"
+          pixBuf <- getPixBufFromInfo info
+          img <- Gtk.imageNew
+          Gtk.imageSetFromPixbuf img pixBuf
+          Gtk.widgetShowAll img
+          Gtk.boxPackStart box img Gtk.PackNatural 0
+      updateHandler _ _ = return ()
+      getPixBufFromInfo ItemInfo { iconPixmaps = pixmaps } = do
+        let (width, height, pixmap) = head pixmaps
+            pixelsPerRow = fromIntegral width
+            bytesPerPixel = 4
+            rowStride = pixelsPerRow * bytesPerPixel
+            finish ptr = do
+              Gtk.pixbufNewFromData (castPtr ptr) colorspace hasAlpha sampleBits
+                   (fromIntegral width) (fromIntegral height) rowStride
+        unsafeUseAsCString pixmap finish
+  join $ H.build H.defaultParams { uniqueIdentifier = "taffybar"
+                                 , handleUpdate = updateHandler
+                                 }
+  return $ Gtk.toWidget box
