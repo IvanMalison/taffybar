0f57e29821f4d2255af78fec8b762223589dedef
Ivan Malison <IvanMalison@gmail.com>
2018-03-16 01:54:15 -0700

    Make Items specified through name work
    Things are still in a very rough state.

--- src/System/Taffybar/Widgets/StatusNotifierTray.hs
+++ src/System/Taffybar/Widgets/StatusNotifierTray.hs
@@ -1,15 +1,17 @@
 module System.Taffybar.Widgets.StatusNotifierTray where
 
 import           Control.Monad
-import qualified Data.ByteString as BS
+import           Control.Monad.Trans
+import           Control.Monad.Trans.Maybe (MaybeT(..))
 import           Data.ByteString.Unsafe
-import           Data.Word
-import           Foreign.C.Types (CUChar(..))
-import           Foreign.Marshal.Array
+import qualified Data.Text as T
 import           Foreign.Ptr
-import           Foreign.Storable
 import qualified Graphics.UI.Gtk as Gtk
+import           Graphics.UI.Gtk.Gdk.Display
+import           Graphics.UI.Gtk.General.IconTheme
 import           StatusNotifier.Host.Service as H
+import           Text.Printf
+import           XMonad.Core (whenJust)
 
 sampleBits :: Int
 sampleBits = 8
@@ -19,6 +21,26 @@ hasAlpha = True
 
 colorspace :: Gtk.Colorspace
 colorspace = Gtk.ColorspaceRgb
+
+getIconByName :: Int -> String -> Maybe FilePath -> IO (Maybe Gtk.Pixbuf)
+getIconByName size name themePath = do
+  putStrLn "hey im here2222"
+  newTheme <- iconThemeNew
+  defaultTheme <- iconThemeGetDefault
+  -- (filePaths, _) <- iconThemeGetSearchPath defaultTheme
+  -- whenJust themePath $
+  --          iconThemeAppendSearchPath newTheme
+  -- mapM_ (iconThemeAppendSearchPath newTheme) filePaths
+  let newTheme = defaultTheme
+  _ <- runMaybeT $ do
+      d <- MaybeT displayGetDefault
+      s <- lift $ displayGetScreen d 0
+      lift $ iconThemeSetScreen newTheme s
+  let panelName = printf "%s-panel" name
+  hasPanelIcon <- iconThemeHasIcon newTheme panelName
+  let targetName = if hasPanelIcon then panelName else name
+      nameText = T.pack targetName
+  iconThemeLoadIcon newTheme nameText size (IconLookupGenericFallback)
 
 buildTray :: IO Gtk.Widget
 buildTray = do
@@ -33,11 +55,17 @@ buildTray = do
           Gtk.boxPackStart box img Gtk.PackNatural 0
       updateHandler IconUpdated info =
         do
-          putStrLn "Handling icon update"
-          pixBuf <- getPixBufFromInfo info
+          -- putStrLn "Handling icon update"
+          -- pixBuf <- getPixBufFromInfo info
           return ()
+      updateHandler NameUpdated info = return ()
       updateHandler _ _ = return ()
-      getPixBufFromInfo ItemInfo { iconPixmaps = pixmaps } = do
+      getPixBufFromInfo info = do
+        maybePB <- getPixBufFromName info
+        maybe (getPixBufFromPixmaps info) return maybePB
+      getPixBufFromName ItemInfo { iconName = name, iconThemePath = themePath } =
+        getIconByName 25 name themePath
+      getPixBufFromPixmaps ItemInfo { iconPixmaps = pixmaps } = do
         let (width, height, pixmap) = head pixmaps
             pixelsPerRow = fromIntegral width
             bytesPerPixel = 4
@@ -46,7 +74,8 @@ buildTray = do
               Gtk.pixbufNewFromData (castPtr ptr) colorspace hasAlpha sampleBits
                    (fromIntegral width) (fromIntegral height) rowStride
         unsafeUseAsCString pixmap finish
-  join $ H.build H.defaultParams { uniqueIdentifier = "taffybar"
-                                 , handleUpdate = (fmap . fmap) Gtk.postGUIAsync updateHandler
-                                 }
+  _ <- join $ H.build H.defaultParams
+       { uniqueIdentifier = "taffybar"
+       , handleUpdate = (fmap . fmap) Gtk.postGUIAsync updateHandler
+       }
   return $ Gtk.toWidget box
