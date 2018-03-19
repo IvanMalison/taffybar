732016e90b8746a9a0d8005d67e0cb873449536a
Ivan Malison <IvanMalison@gmail.com>
2018-03-19 16:56:09 -0700

    Add (likely failed) dbus menu widget

--- src/System/Taffybar/Widgets/StatusNotifierTray.hs
+++ src/System/Taffybar/Widgets/StatusNotifierTray.hs
@@ -1,15 +1,22 @@
 module System.Taffybar.Widgets.StatusNotifierTray where
 
+import           Control.Concurrent.MVar as MV
 import           Control.Monad
 import           Control.Monad.Trans
 import           Control.Monad.Trans.Maybe (MaybeT(..))
+import           Control.Monad.Trans.Reader
+import           DBus.Client
 import           Data.ByteString.Unsafe
+import           Data.Int
+import qualified Data.Map.Strict as Map
+import           Data.Maybe
 import qualified Data.Text as T
 import           Foreign.Ptr
 import qualified Graphics.UI.Gtk as Gtk
 import           Graphics.UI.Gtk.Gdk.Display
 import           Graphics.UI.Gtk.General.IconTheme
 import           StatusNotifier.Host.Service as H
+import qualified StatusNotifier.Menu.Service as M
 import           Text.Printf
 import           XMonad.Core (whenJust)
 
@@ -27,6 +34,7 @@ getIconByName size name themePath = do
   putStrLn "hey im here2222"
   newTheme <- iconThemeNew
   defaultTheme <- iconThemeGetDefault
+  -- TODO: make theming work
   -- (filePaths, _) <- iconThemeGetSearchPath defaultTheme
   -- whenJust themePath $
   --          iconThemeAppendSearchPath newTheme
@@ -42,30 +50,79 @@ getIconByName size name themePath = do
       nameText = T.pack targetName
   iconThemeLoadIcon newTheme nameText size (IconLookupGenericFallback)
 
+data ItemContext = ItemContext
+  { contextInfo :: H.ItemInfo
+  , menuMVar :: MV.MVar (Map.Map Int32 M.MenuEntry)
+  , contextImage :: Gtk.Image
+  , contextWidget :: Gtk.EventBox
+  }
+
+fillMenu :: Gtk.MenuClass menu => ItemContext ->  menu -> IO ()
+fillMenu ItemContext { menuMVar = menuVar } menu = do
+  menuEntryMap <- MV.readMVar menuVar
+  putStrLn "hey"
+  print menuEntryMap
+  let mroot = Map.lookup 0 menuEntryMap
+      childrenIDs = fromMaybe [] $ M.childIDs <$> mroot
+      childrenEntries = catMaybes $ map (flip Map.lookup menuEntryMap) childrenIDs
+      addItemForEntry M.MenuEntry { M.label = labelText } =
+        Gtk.menuItemNewWithLabel labelText >>= Gtk.menuShellAppend menu
+  mapM_ addItemForEntry childrenEntries
+
 buildTray :: IO Gtk.Widget
 buildTray = do
+  client <- connectSession
   box <- Gtk.hBoxNew False 5
-  let updateHandler ItemAdded info =
+  widgetMap <- MV.newMVar Map.empty
+
+  let getContext name = Map.lookup name <$> MV.readMVar widgetMap
+
+      updateHandler ItemAdded
+                    info@ItemInfo { menuPath = pathForMenu
+                                  , itemServiceName = serviceName
+                                  } =
         do
-          putStrLn "Handling new item"
           pixBuf <- getPixBufFromInfo info
           img <- Gtk.imageNew
+          ebox <- Gtk.eventBoxNew
           Gtk.imageSetFromPixbuf img pixBuf
-          Gtk.widgetShowAll img
-          Gtk.boxPackStart box img Gtk.PackNatural 0
-      updateHandler IconUpdated info =
-        do
-          -- putStrLn "Handling icon update"
-          -- pixBuf <- getPixBufFromInfo info
-          return ()
-      updateHandler NameUpdated info = return ()
+          Gtk.containerAdd ebox img
+          Gtk.widgetShowAll ebox
+          Gtk.boxPackStart box ebox Gtk.PackNatural 0
+          (menuVar, _) <- runReaderT (M.buildMenu M.defaultMenuParams)
+                          (client, serviceName, pathForMenu)
+          menu <- Gtk.menuNew
+          putStrLn "yooooo"
+          let context =
+                ItemContext { contextInfo = info
+                            , menuMVar = menuVar
+                            , contextImage = img
+                            , contextWidget = ebox
+                            }
+          fillMenu context menu
+          Gtk.menuAttachToWidget menu ebox
+          Gtk.on ebox Gtk.buttonPressEvent $ liftIO $
+             Gtk.menuPopup menu Nothing >> return True
+          MV.modifyMVar_ widgetMap $ return . (Map.insert serviceName context)
+
+      updateHandler ItemRemoved ItemInfo { itemServiceName = name }
+        = putStrLn "here" >> getContext name >>= removeWidget
+        where removeWidget Nothing = putStrLn "Tried to remove widget for which we have no icon"
+              removeWidget (Just (ItemContext { contextWidget = widgetToRemove })) =
+                do
+                  putStrLn "removing widget"
+                  Gtk.containerRemove box widgetToRemove
+                  MV.modifyMVar_ widgetMap $ return . (Map.delete name)
+
       updateHandler _ _ = return ()
+
       getPixBufFromInfo info = do
         maybePB <- getPixBufFromName info
         maybe (getPixBufFromPixmaps info) return maybePB
       getPixBufFromName ItemInfo { iconName = name, iconThemePath = themePath } =
         getIconByName 25 name themePath
       getPixBufFromPixmaps ItemInfo { iconPixmaps = pixmaps } = do
+              -- TODO: get rid of the head here
         let (width, height, pixmap) = head pixmaps
             pixelsPerRow = fromIntegral width
             bytesPerPixel = 4
@@ -74,8 +131,11 @@ buildTray = do
               Gtk.pixbufNewFromData (castPtr ptr) colorspace hasAlpha sampleBits
                    (fromIntegral width) (fromIntegral height) rowStride
         unsafeUseAsCString pixmap finish
+
   _ <- join $ H.build H.defaultParams
        { uniqueIdentifier = "taffybar"
+       -- TODO: more fine grained approach to making sure things are on the UI thread
        , handleUpdate = (fmap . fmap) Gtk.postGUIAsync updateHandler
+       , dbusClient = Just client
        }
   return $ Gtk.toWidget box
